31bff4711aa7ec77248bffe2e80a01dfe14e4a9c
PietroBossolasco <p.bossolasco.1715@vallauri.edu>
2023-10-29 17:20:53 +0100

    Fixed bug on multi init

--- source/text-selector.js
+++ source/text-selector.js
@@ -1,97 +1,93 @@
-// text-selector V0.1.0 - Made by Pietro Bossolasco
-function textselectorinit(p) {
-  s = document.createElement("div");
-  s.classList.add("text-selector-selector");
-  s.addEventListener("mouseleave", function () {
-    canc=false;
-    s.addEventListener("mouseover", () => {
-        canc=true;
-    })
-    setTimeout(() => {
-        if(!canc){
-            try{
-                $(".text-selector-selector").eq(0).fadeOut(100);
-            }catch(ex){
-                this.style.display = "none";
+// text-selector V0.1.1 - Made by Pietro Bossolasco
+// For docs -> https://github.com/PietroBossolasco/text-selector
+function textselectorinit(e) {
+  (s = document.createElement("div")).classList.add("text-selector-selector"),
+    s.addEventListener("mouseleave", function () {
+      (canc = !1),
+        s.addEventListener("mouseover", () => {
+          canc = !0;
+        }),
+        setTimeout(() => {
+          if (!canc)
+            try {
+              $(".text-selector-selector").eq(0).fadeOut(100);
+            } catch (e) {
+              this.style.display = "none";
             }
+          canc = !1;
+        }, 500);
+    }),
+    document.getElementsByTagName("body")[0].appendChild(s);
+  for (let l = 0; l < e.length; l++) {
+    let i = l;
+    Object.freeze(i),
+      (t = document.getElementById(e[l].text)).classList.add(
+        "text-selector-text"
+      ),
+      -1 !== (d = document.getElementById(e[l].data)).selectedIndex &&
+        (t.innerText = d.options[d.selectedIndex].text),
+      (d.style.display = "none");
+    try {
+      t.addEventListener("click", function () {
+        if (!(d = document.getElementById(e[i].data)).disabled) {
+          (t = document.getElementById(e[i].text)),
+            (c = d.childNodes),
+            (aus = Array.from(c)),
+            (s.innerHTML = "");
+          var l = document.createElement("div");
+          l.classList.add("text-selector-content"),
+            c.forEach(function (e) {
+              e.innerText &&
+                ((sel = document.createElement("div")),
+                e.disabled
+                  ? (sel.classList =
+                      "text-selector-selection text-selector-disabled")
+                  : e.selected
+                  ? (sel.classList =
+                      "text-selector-selection text-selector-selected")
+                  : (sel.classList =
+                      "text-selector-selection text-selector-normal"),
+                (sel.innerText = e.innerText),
+                e.disabled ||
+                  sel.addEventListener("click", function () {
+                    (ex = document.getElementsByClassName(
+                      "text-selector-selected"
+                    )),
+                      null != ex[0] &&
+                        (ex[0].classList =
+                          "text-selector-selection text-selector-normal"),
+                      (v = this.innerText),
+                      (this.classList =
+                        "text-selector-selection text-selector-selected"),
+                      (d.selectedIndex = aus.indexOf(e) - 1),
+                      (t.innerText = v);
+                  }),
+                l.appendChild(sel));
+            }),
+            (s.innerHTML = ""),
+            s.appendChild(l);
+          try {
+            $(".text-selector-selector").eq(0).fadeIn(100);
+          } catch (n) {
+            s.style.display = "block";
+          }
+          let o = this.offsetLeft,
+            r = this.offsetTop;
+          r > s.offsetHeight
+            ? ((s.style.paddingBottom = this.offsetHeight + 10 + "px"),
+              (s.style.top = r - s.offsetHeight + this.offsetHeight + "px"),
+              (s.style.paddingTop = "10px"))
+            : ((s.style.paddingBottom = "10px"),
+              (s.style.top = this.offsetHeight + "px"),
+              (s.style.paddingTop = this.offsetHeight + 10 + "px")),
+            o > s.offsetWidth / 2 + 10
+              ? (s.style.left =
+                  o + this.offsetWidth / 2 - s.offsetWidth / 2 + "px")
+              : (s.style.left = "10px");
         }
-        canc=false;
-    }, 500);
-  });
-  document.getElementsByTagName("body")[0].appendChild(s);
-  for (i = 0; i < p.length; i++) {
-    t = document.getElementById(p[i].text);
-    d = document.getElementById(p[i].data);
-    if (d.selectedIndex !== -1) {
-        t.innerText = d.options[d.selectedIndex].text;
-    }
-    d.style.display="none";
-    try{
-        t.addEventListener("click", function () {
-            if(!d.disabled){
-                c = d.childNodes;
-                aus=Array.from(c);
-                s.innerHTML='';
-                content=document.createElement("div");
-                content.classList.add('text-selector-content');
-                c.forEach(e => {
-                    if(e.innerText){
-                        sel=document.createElement('div');
-                        if(e.disabled){
-                            sel.classList='text-selector-selection text-selector-disabled';
-                        }
-                        else if(e.selected){
-                            sel.classList='text-selector-selection text-selector-selected';
-                        }
-                        else{
-                            sel.classList='text-selector-selection text-selector-normal';
-                        }
-                        sel.innerText = e.innerText;
-                        if(!e.disabled){
-                            sel.addEventListener("click", function(){
-                                ex=document.getElementsByClassName('text-selector-selected');
-                                if (ex[0] != null) {
-                                    ex[0].classList='text-selector-selection text-selector-normal';
-                                }
-                                v=this.innerText;
-                                this.classList='text-selector-selection text-selector-selected';
-                                e.selected=true;
-                                t.innerText=v;
-                            })
-                        }
-                        content.appendChild(sel)
-                    }
-                });
-                s.appendChild(content);
-                try{
-                    $(".text-selector-selector").eq(0).fadeIn(100);
-                }catch(ex){
-                    s.style.display = "block";
-                }
-                const x = this.offsetLeft;
-                const y = this.offsetTop;
-                if(y>s.offsetHeight){
-                    s.style.paddingBottom = (this.offsetHeight+10)+'px';
-                    s.style.top = (y-s.offsetHeight+(this.offsetHeight)) + "px";
-                    s.style.paddingTop = '10px';
-                }
-                else{
-                    s.style.paddingBottom='10px';
-                    s.style.top=this.offsetHeight + "px";
-                    s.style.paddingTop = (this.offsetHeight+10)+'px';
-                }
-                if(x>((s.offsetWidth/2)+10)){
-                    s.style.left = ((x+(this.offsetWidth/2))-(s.offsetWidth/2)) + "px";
-                }
-                else{
-                    s.style.left = "10px";
-                }
-            }
-        });
-    }
-    catch(ex){
-        alert('Error in the text-selector configuration');
-        console.error(ex);
+      });
+    } catch (n) {
+      alert("Error in the text-selector configuration"), console.error(n);
     }
   }
 }
